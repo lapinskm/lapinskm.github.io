898cd29fa43039231ff9993d5b1575196309ff2c
Marcin Łapiński <lapinskm@gmail.com>
2026-05-24 21:12:13 +0200

    simplified gallery image size

--- html/gallery.js
+++ html/gallery.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 	var viewportScale = 0.7; // fraction of viewport width used for images
-	var zoomFactor = window.devicePixelRatio
-	var w = Math.round(window.innerWidth * viewportScale * zoomFactor) + "px";
+	var w = Math.round(window.innerWidth * viewportScale) + "px";
 	document.querySelectorAll(".gallery-img").forEach(function(el) {
 		el.style.width = w;
 		el.style.margin = "0 auto";
