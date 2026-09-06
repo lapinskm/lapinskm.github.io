ba5745a8301da8b690f5f7a38624580f848074b4
Marcin Łapiński <lapinskm@gmail.com>
2026-09-06 15:42:01 +0200

    Inserted part III_VITAE

--- html/gallery.js
+++ html/gallery.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 	var viewportScale = 0.7; // fraction of viewport width used for images
-	var w = Math.round(window.innerWidth * viewportScale) + "px";
+	var zoomFactor = window.devicePixelRatio
+	var w = Math.round(window.innerWidth * viewportScale * zoomFactor) + "px";
 	document.querySelectorAll(".gallery-img").forEach(function(el) {
 		el.style.width = w;
 		el.style.margin = "0 auto";
